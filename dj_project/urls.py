41c80b38d9986629e1f0ecb0eb291d2d3a730e92
schesnowitz <steve@margoscargo.com>
2023-09-24 18:00:59 -0400

    index page

--- dj_project/urls.py
+++ dj_project/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import some_page
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', some_page),
+    path('', include('pages.urls')),
 
 ]
